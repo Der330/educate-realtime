f0e35d9f86035efd6b720c83503c9a7b1355ee8e
okk <20104050126@csiic.edu.cn>
2024-09-11 20:46:15 +0800

    加入SqlUtil

---
+++ realtime-common/src/main/java/com/atguigu/educate/realtime/common/util/SqlUtil.java
@@ -0,0 +1,91 @@
+package com.atguigu.educate.realtime.common.util;
+
+
+import com.atguigu.educate.realtime.common.constant.Constant;
+
+public class SqlUtil {
+    public static String kafkaConnector(String topic, String groupId) {
+        String sql =
+                "WITH (\n" +
+                "  'connector' = 'kafka',\n" +
+                "  'topic' = '"+topic+"',\n" +
+                "  'properties.bootstrap.servers' = '"+ Constant.KAFKA_BROKERS +"',\n" +
+                "  'properties.group.id' = '"+groupId+"',\n" +
+                "  'scan.startup.mode' = 'latest-offset',\n" +
+                "  'format' = 'json'\n" +
+                ")";
+        return sql;
+    }
+
+    public static String upsertKafkaConnector(String topic, int parallelism) {
+        String sql =
+                "WITH (\n" +
+                "  'connector' = 'upsert-kafka',\n" +
+                "  'topic' = '"+topic+"',\n" +
+                "  'properties.bootstrap.servers' = '"+ Constant.KAFKA_BROKERS +"',\n" +
+                "  'key.format' = 'json',\n" +
+                "  'value.format' = 'json',\n" +
+                "  'sink.parallelism' = '"+parallelism+"'\n" +
+                ")";
+        return sql;
+    }
+
+    //获取HBase连接器相关连接属性
+    public static String hBaseConnector(String tableName){
+        return " WITH (\n" +
+                " 'connector' = 'hbase-2.2',\n" +
+                " 'table-name' = '"+Constant.HBASE_NAMESPACE+":"+tableName+"',\n" +
+                " 'zookeeper.quorum' = 'hadoop102,hadoop103,hadoop104:2181',\n" +
+                " 'lookup.async' = 'true',\n" +
+                " 'lookup.cache' = 'PARTIAL',\n" +
+                " 'lookup.partial-cache.max-rows' = '200',\n" +
+                " 'lookup.partial-cache.expire-after-write' = '1 hour',\n" +
+                " 'lookup.partial-cache.expire-after-access' = '1 hour'\n" +
+                ")";
+    }
+
+    public static String dorisConnector(String tableName){
+
+        String sql = "WITH (\n" +
+                "  'connector' = 'doris', " +
+                "  'fenodes' = '"+Constant.DORIS_FE_NODES+"', " +
+                "  'table.identifier' = '"+Constant.DORIS_DATABASE+"."+tableName+"', " +
+                "  'username' = 'root', " +
+                "  'password' = '"+Constant.DORIS_PASSWORD+"', " +
+                "  'sink.properties.format' = 'json', " +
+                "  'sink.enable-2pc' = 'false' " + // 测试阶段可以关闭两阶段提交,方便测试
+                ")";
+
+        return sql;
+    }
+
+    public static String readOdsDbSql(String groupId){
+        String sql =
+                "CREATE TABLE topic_db (\n" +
+                        "  `database` string,\n" +
+                        "  `table` string,\n" +
+                        "  `type` string,\n" +
+                        "  `data` map<string,string>,\n" +
+                        "  `old` map<string,string>,\n" +
+                        "  ts bigint,\n" +
+                        "  pt as proctime(),\n" +
+                        "  et as TO_TIMESTAMP_LTZ(ts, 0),\n" +
+                        "  WATERMARK FOR et AS et \n" +
+                        ") " + kafkaConnector(Constant.TOPIC_DB,groupId);
+        return sql;
+    }
+
+    public  static String readDicSql(String tableName) {
+        String sql = "CREATE TABLE base_dic (\n" +
+                " dic_code string,\n" +
+                " info ROW<dic_name string>,\n" +
+                " PRIMARY KEY (dic_code) NOT ENFORCED\n" +
+                ") " + hBaseConnector(tableName);
+
+        return sql;
+        //tableEnv.executeSql("select dic_code,dic_name from base_dic").print();
+    }
+
+
+
+}
--- realtime-dim/src/main/java/com/atguigu/educat/realtime/dim/app/DimApp.java
+++ realtime-dim/src/main/java/com/atguigu/educat/realtime/dim/app/DimApp.java
@@ -5,21 +5,60 @@ import com.alibaba.fastjson.JSONObject;
 import com.atguigu.educate.realtime.common.constant.Constant;
 import com.atguigu.educate.realtime.common.util.FlinkEnvUtil;
 import com.atguigu.educate.realtime.common.util.FlinkKafkaUtil;
+import com.ververica.cdc.connectors.mysql.source.MySqlSource;
+import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.util.Collector;
+
+import java.util.Properties;
 
 public class DimApp {
     public static void main(String[] args) throws Exception {
 
-        StreamExecutionEnvironment env=FlinkEnvUtil.getEnv(18080, 4);
+        StreamExecutionEnvironment env=FlinkEnvUtil.getEnv(10002, 4);
 
         DataStreamSource<String> sourceDS=env.fromSource(FlinkKafkaUtil.getKafkaSource(Constant.TOPIC_DB,"DimApp"), WatermarkStrategy.noWatermarks(), "DimApp");
 
         //转换为JSONObject
         SingleOutputStreamOperator<JSONObject> jsonObjDS=sourceDS.map(JSON::parseObject);
-        jsonObjDS.print();
+        SingleOutputStreamOperator<JSONObject> filterDS=jsonObjDS.process(new ProcessFunction<JSONObject, JSONObject>() {
+            @Override
+            public void processElement(JSONObject jsonObject, ProcessFunction<JSONObject, JSONObject>.Context context, Collector<JSONObject> collector) throws Exception {
+
+                String database=jsonObject.getString("database");
+                String data=jsonObject.getString("data");
+
+                if ("edu".equals(database) && data.length() > 2) {
+                    collector.collect(jsonObject);
+                }
+
+            }
+        });
+
+
+        //获得维度配置信息表
+        Properties properties=new Properties();
+        properties.setProperty("useSSL", "false");
+        properties.setProperty("allowPublicKeyRetrieval", "true");
+
+        MySqlSource<String> mySqlSource=MySqlSource.<String>builder()
+                .hostname("localhost")
+                .port(13306)
+                .jdbcProperties(properties)
+                .username(Constant.MYSQL_USER_NAME)
+                .password(Constant.MYSQL_PASSWORD)
+                .databaseList("edu_config")
+                .tableList("edu_config.table_process_dim")
+                .deserializer(new JsonDebeziumDeserializationSchema())
+                .build();
+
+        DataStreamSource<String> mysqlSourceDS=env.fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "mysqlCDCSource");
+
+
 
 
         env.execute();
