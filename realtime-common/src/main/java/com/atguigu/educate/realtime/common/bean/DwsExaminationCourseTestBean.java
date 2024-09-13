f1826058c451d076f60958d683f573fc65bbb763
xim <1437620346@qq.com>
2024-09-13 13:46:23 +0800

    course

---
+++ realtime-common/src/main/java/com/atguigu/educate/realtime/common/bean/DwsExaminationCourseTestBean.java
@@ -0,0 +1,28 @@
+package com.atguigu.educate.realtime.common.bean;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class DwsExaminationCourseTestBean {
+    String stt;
+    String edt;
+    String curDate;
+    String courseId;
+    String courseName;
+    Long UserCt;
+    BigDecimal scoreSum;
+    Long durationSum;
+
+    @JSONField(serialize = false)
+    String userId;
+
+    @JSONField(serialize = false)
+    Long ts;
+}
